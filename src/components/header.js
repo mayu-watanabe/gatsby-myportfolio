47d0ea9ea2d01e36c8abdd87279a758aeabbc87a
mayu-watanabe <mayu.watanabe.1994@gmail.com>
2021-04-29 15:18:51 +0900

    apply scroll animation for nav

--- src/components/header.js
+++ src/components/header.js
@@ -2,23 +2,49 @@ import * as React from "react"
 import PropTypes from "prop-types"
 import { Link } from "gatsby"
 import * as Style from "../styles/components/header.module.scss"
+import { gsap } from 'gsap'
+import { ScrollTrigger } from 'gsap/ScrollTrigger'
 
-const Header = ({ siteTitle }) => (
-  <header className={Style.header}>
-    <div className={Style.header__container}>
-      <div className={Style.header__siteName}>
-        <Link
-          to="/"
-          style={{
-            textDecoration: `none`,
-          }}
-        >
-          {siteTitle}
-        </Link>
+const Header = ({ siteTitle }) => {
+  React.useEffect(() => {
+    gsap.registerPlugin(ScrollTrigger)
+    setAnimation()
+  }, [])
+
+  const setAnimation = () => {
+    gsap.fromTo('.nav',
+      { opacity: 0 },
+      {
+        opacity: 1,
+        scrollTrigger: {
+          trigger: 'section:nth-child(2)',
+          start: 'top top',
+          end: 'top bottom',
+          onEnter: () => {},
+          onEnterBack: () => {},
+          scrub: 1
+        }
+      }
+    )
+  }
+
+  return (
+    <header className={Style.header}>
+      <div className={Style.header__container + " " + "nav"}>
+        <div className={Style.header__siteName}>
+          <Link
+            to="/"
+            style={{
+              textDecoration: `none`,
+            }}
+          >
+            {siteTitle}
+          </Link>
+        </div>
       </div>
-    </div>
-  </header>
-)
+    </header>
+  )
+}
 
 Header.propTypes = {
   siteTitle: PropTypes.string,
